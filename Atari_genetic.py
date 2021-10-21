517100cfa3f2c522b8c6570594662ab0db9f4b1b
Amanjot Bhullar <bhullara@gra-login3.graham.sharcnet>
2021-10-20 20:52:43 -0400

    updating to bring to another computer

--- Atari_genetic.py
+++ Atari_genetic.py
@@ -19,15 +19,18 @@ def evaluate(game,seed,mut,env,avg_rewards,i):
     test_size = params[3]
     poolsizes = params[4]
     numconvlayers = params[5]
+
+    print("Making network")
     torch.manual_seed(seed)
     atarinet = AtariNetCONV(inshape=inshape,
                             poolsizes = poolsizes,
                             numconvlayers = numconvlayers, 
                             outsize = numactions).eval()
 
+    print("mutating network")
     #apply mutations
     atarinet.mutate(mut_power,mut)
-    
+    print("beginning test")
     total_rewards = []
     for i_episode in range(test_size):
         #print(i,i_episode)
@@ -56,9 +59,11 @@ def evaluate(game,seed,mut,env,avg_rewards,i):
             rewards.append(reward)
         total_rewards.append(np.sum(rewards))
     #print("finished ind:",i)
+    print("updating avg rewards")
     avg_rewards[i] = np.mean(total_rewards)
+    print(avg_rewards)
 
-def select_and_mutate(population,mutations,avg_rewards,pop_size):
+def select_and_mutate(population,mutations,avg_rewards,pop_size,trunc):
     fit_order = np.argsort(avg_rewards)[::-1] #best to worst
 
     print("Average rewards:\t",np.mean(avg_rewards))
@@ -66,9 +71,13 @@ def select_and_mutate(population,mutations,avg_rewards,pop_size):
     print("Max reward:\t\t",avg_rewards[fit_order[0]])
     print() 
 
+    #Probabilistic selection based off of rewards received
     #selection_probs = np.exp(avg_rewards[fit_order])/np.sum(np.exp(avg_rewards))
     #survivors = np.random.choice(pop_size+arch_size,pop_size,p = selection_probs)
-    arch_size = len(populations) - pop_size
+
+    #cutoff selection
+    survivors = fit_order[np.random.randint(0,trunc,size = pop_size)]
+    arch_size = len(population) - pop_size
     survivors = np.append(survivors,fit_order[:arch_size])
     population = [population[i] for i in survivors]    
 
@@ -91,9 +100,10 @@ def game_params(game):
         numactions = 18
         inshape = [176,42]
         mut_power = 0.002
-        test_size = 30
+        test_size = 3
         poolsizes = [3,2] #3 makes sense since tetris blocks are 3 pixels wide
         numconvlayers = 6 #makes field of vision of last layer full width of processed frame
+        trunc = 3
 
     elif game == 'DemonAttack':
         numactions = 6
@@ -102,16 +112,19 @@ def game_params(game):
         test_size = 30
         poolsizes = [2,2]
         numconvlayers = 10
+        trunc = 50
 
-    return [numactions,inshape,mut_power,test_size,poolsizes,numconvlayers]
+    return [numactions,inshape,mut_power,test_size,poolsizes,numconvlayers,trunc]
 
 if __name__ == "__main__":
     game = "Tetris"
     almightyint = 6671111 #call pizza pizza hey hey hey
-    pop_size = 500
-    arch_size = 10
-    num_gens = 1
+    pop_size = 5
+    arch_size = 1
+    num_gens = 2
     cur_gen = 0
+    params = game_params(game)
+    trunc = params[-1]
 
     ale = ALEInterface()
     env = gym.make('ALE/{}-v5'.format(game), render_mode='rgb_array')
@@ -133,8 +146,10 @@ if __name__ == "__main__":
             evaluate(game,seed,mut,env,avg_rewards,i)
 
         #save here so we can reconstruct the next generation if need be
+        print("saving...")
         np.savez("{}/GAseeds_{}_nmp_{}.npz".format(game,game,gen+1),[population,mutations,avg_rewards],allow_pickle = True)
-        select_and_mutate(population,mutations,avg_rewards,pop_size)
+        print("selecting next gen...")
+        select_and_mutate(population,mutations,avg_rewards,pop_size,trunc)
 
 
 
--- runGA.sh
+++ runGA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=10:00:00
+#SBATCH --time=00:10:00
 #SBATCH --account=def-aali
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=16G
